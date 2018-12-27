57a48c682c3b22501a4de85fb6a41cb88d89a98f
Jeremy Miller <migimasa@gmail.com>
2018-12-27 15:57:36 -0600

    Begin testing series service

---
+++ SubscriptionManager/SubscriptionManager.Services.Tests/Mocks/MockSeriesDataAccess.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SubscriptionManager.Services.Tests.Mocks
+{
+    class MockSeriesDataAccess
+    {
+    }
+}
---
+++ SubscriptionManager/SubscriptionManager.Services.Tests/SeriesServiceTests.cs
@@ -0,0 +1,29 @@
+﻿using SubscriptionManager.Core.DTO;
+using SubscriptionManager.Services.Implementations;
+using SubscriptionManager.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SubscriptionManager.Services.Tests
+{
+    
+    public class SeriesServiceTests
+    {
+        [Fact]
+        public void EmptyTest()
+        {
+            Assert.True(true);
+        }
+
+        [Fact]
+        public async Task GetAllComicBookSeries()
+        {
+            ISeriesService service = new SeriesService();
+
+            IEnumerable<ComicBookSeries> issues = service.GetIssues();
+        }
+    }
+}
