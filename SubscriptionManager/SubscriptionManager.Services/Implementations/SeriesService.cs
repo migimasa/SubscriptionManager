b11f4e75856d1520c6c1eab9b772ebc1157325ba
Jeremy Miller <migimasa@gmail.com>
2018-12-27 15:56:43 -0600

    Add series service

---
+++ SubscriptionManager/SubscriptionManager.Services/Implementations/SeriesService.cs
@@ -0,0 +1,11 @@
+﻿using SubscriptionManager.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SubscriptionManager.Services.Implementations
+{
+    public class SeriesService : ISeriesService
+    {
+    }
+}
---
+++ SubscriptionManager/SubscriptionManager.Services/Interfaces/ISeriesService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SubscriptionManager.Services.Interfaces
+{
+    public interface ISeriesService
+    {
+        IEnumerable<Core.DTO.ComicBookSeries> GetIssues();
+    }
+}
