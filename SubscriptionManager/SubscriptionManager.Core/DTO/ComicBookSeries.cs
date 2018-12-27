08eec810f66835e5096d62008011b786b5a4d8f8
Jeremy Miller <migimasa@gmail.com>
2018-12-27 15:55:46 -0600

    Add Core Project

---
+++ SubscriptionManager/SubscriptionManager.Core/DTO/ComicBookSeries.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SubscriptionManager.Core.DTO
+{
+    public class ComicBookSeries
+    {
+        public int ComicBookSeriesId { get; set; }
+        public string ComicBookSeriesName { get; set; }
+    }
+}
